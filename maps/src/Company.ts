8b236d97eb2330423aead75aaf3fbdd905bd33b9
albaye <alba.ye99@gmail.com>
2023-09-30 21:42:09 +0100

    Map application

---
+++ maps/src/Company.ts
@@ -0,0 +1,29 @@
+import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
+
+export class Company implements Mappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>Company Name: ${this.name}</h1>
+      <h3>Catch phrase: ${this.catchPhrase}</h3>
+    </div>
+    `;
+  }
+}
---
+++ maps/src/index.ts
@@ -0,0 +1,11 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
+import { CustomMap } from "./CustomMap";
+
+const customMap = new CustomMap("map");
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
