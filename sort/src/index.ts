562dff7e90d08cddb305be6a212686e0a51b2881
albaye <alba.ye99@gmail.com>
2023-10-01 14:56:10 +0100

    Sorter

---
+++ sort/src/index.ts
@@ -0,0 +1,18 @@
+import { NumbersColletion } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+const numbersCollection = new NumbersColletion([1, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("xaa");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
