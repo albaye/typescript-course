96f98c826870e559667012d9ac63a82985bc3601
albaye <alba.ye99@gmail.com>
2023-12-09 20:48:31 +0000

    Putting it All Togather

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,8 @@
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analysers/WinsAnalysis";
+import { Summary } from "./Summary";
 
 // Create an object that satisfies the "DateReader" interface
 const reader = new CsvFileReader("football.csv");
@@ -8,3 +10,10 @@ const reader = new CsvFileReader("football.csv");
 // Create an instance of MatchReader and pass in something satisfying the "DataReader" interface
 const matchReader = new MatchReader(reader);
 matchReader.load();
+
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
