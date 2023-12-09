f8f79eaaa6b2e19ce18b623e7b416f699f27add4
albaye <alba.ye99@gmail.com>
2023-12-09 20:57:52 +0000

    Statis methods

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,13 +5,9 @@ import { WinsAnalysis } from "./analysers/WinsAnalysis";
 import { Summary } from "./Summary";
 import { HtmlReport } from "./reportTargets/HtmlReport";
 
-// Create an object that satisfies the "DateReader" interface
-const reader = new CsvFileReader("football.csv");
-
-// Create an instance of MatchReader and pass in something satisfying the "DataReader" interface
-const matchReader = new MatchReader(reader);
+const matchReader = MatchReader.fromCsv("football.csv");
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
 summary.buildAndPrintReport(matchReader.matches);
